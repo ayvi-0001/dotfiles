789da21695837307f5b4af07d60f32193648f8f8
ayvi-0001 <alankvickers+github@gmail.com>
2025-02-05 14:50:23 -0800

    wezterm: add cfg_gpu.lua

---
+++ wezterm/cfg_gpu.lua
@@ -0,0 +1,21 @@
+local wezterm = require "wezterm"
+local config = wezterm.config_builder()
+local module = {}
+
+function module.apply_to_config(config)
+  config.front_end= "WebGpu"
+  config.max_fps = 60
+  config.prefer_egl = true
+  config.webgpu_power_preference = "HighPerformance"
+  config.webgpu_preferred_adapter = {
+    backend = "Vulkan",
+    device = 10115,
+    device_type = "DiscreteGpu",
+    driver = "NVIDIA",
+    driver_info = "566.36",
+    name = "NVIDIA GeForce RTX 4070 SUPER",
+    vendor = 4318,
+  }
+end
+
+return module
