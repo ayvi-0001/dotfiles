e33f4f7c3d2e5a784b1bdfd564af445c4e8662d6
ayvi-0001 <alankvickers+github@gmail.com>
2025-02-05 14:50:40 -0800

    wezterm: add wezterm.lua

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require "wezterm"
+local config = wezterm.config_builder()
+
+local init = require "init"
+
+init.cache.apply_to_config(config)
+init.cmdplt.apply_to_config(config)
+init.color.apply_to_config(config)
+init.font.apply_to_config(config)
+init.gpu.apply_to_config(config)
+init.keys.apply_to_config(config)
+init.proc.apply_to_config(config)
+init.term.apply_to_config(config)
+init.window.apply_to_config(config)
+
+return config
