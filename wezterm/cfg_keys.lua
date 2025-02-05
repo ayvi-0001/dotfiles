0a854768e30503d78ca231566108a4f30c4fe9e2
ayvi-0001 <alankvickers+github@gmail.com>
2025-02-05 14:49:48 -0800

    wezterm: add cfg_keys.lua

---
+++ wezterm/cfg_keys.lua
@@ -0,0 +1,36 @@
+local wezterm = require "wezterm"
+local config = wezterm.config_builder()
+local module = {}
+
+function module.apply_to_config(config)
+  config.keys = {
+    {
+      key = "w",
+      mods = "SHIFT|CTRL",
+      action = wezterm.action.CloseCurrentPane { confirm = false },
+    },
+  }
+  config.mouse_bindings = {
+      -- Disable the default click behavior
+      {
+        event = { Up = { streak = 1, button = "Left"} },
+        mods = "NONE",
+        action = wezterm.action.DisableDefaultAssignment,
+      },
+      -- Ctrl-click will open the link under the mouse cursor
+      {
+          event = { Up = { streak = 1, button = "Left" } },
+          mods = "CTRL",
+          action = wezterm.action.OpenLinkAtMouseCursor,
+      },
+      -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+      {
+          event = { Down = { streak = 1, button = "Left" } },
+          mods = "CTRL",
+          action = wezterm.action.Nop,
+      },
+  }
+
+end
+
+return module
