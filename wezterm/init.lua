116bc9ec03b54e6e9c8d9974c67fb99c901e5fce
ayvi-0001 <alankvickers+github@gmail.com>
2025-02-05 14:50:32 -0800

    wezterm: add init.lua

---
+++ wezterm/init.lua
@@ -0,0 +1,13 @@
+cfg = {
+  cache  = require "cfg_cache";
+  cmdplt = require "cfg_cmdplt";
+  color  = require "cfg_color";
+  font   = require "cfg_font";
+  gpu    = require "cfg_gpu";
+  keys   = require "cfg_keys";
+  proc   = require "cfg_proc";
+  term   = require "cfg_term";
+  window = require "cfg_window";
+}
+
+return cfg
