d3f5102efde10cd4a928bf446969f8d9f9898f4e
ayvi-0001 <alankvickers+github@gmail.com>
2025-02-26 20:12:22 -0800

    wezterm(cache): set mux_output_parser_buffer_size

--- wezterm/cfg_cache.lua
+++ wezterm/cfg_cache.lua
@@ -9,6 +9,7 @@ function module.apply_to_config(config)
   config.line_quad_cache_size = 3072
   config.line_state_cache_size = 3072
   config.line_to_ele_shape_cache_size = 3072
+  config.mux_output_parser_buffer_size = 1000000
   config.mux_output_parser_coalesce_delay_ms = 10
   config.periodic_stat_logging = 10
   config.shape_cache_size = 3072
