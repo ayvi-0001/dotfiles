7e7ad52ea82a8e231041905befd0210a1ba0b1d8
ayvi-0001 <alankvickers+github@gmail.com>
2025-03-31 16:26:01 -0700

    wezterm: update default_prog

--- wezterm/cfg_term.lua
+++ wezterm/cfg_term.lua
@@ -9,7 +9,8 @@ local M = {}
 function M.apply_to_config(config)
   config.audible_bell = "Disabled"
   config.automatically_reload_config = false
-  config.default_prog = { "c:/program files/git/bin/bash.exe", "-il" }
+  config.default_mux_server_domain = "local"
+  config.default_prog = { "bash", "-il" }
   config.detect_password_input = false
   config.mux_enable_ssh_agent = true
   config.mux_env_remove = { "SSH_AUTH_SOCK", "SSH_CLIENT", "SSH_CONNECTION" }
