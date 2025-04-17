f4b06d9a400cc8fd44001cc635ee5a03efd92b3a
ayvi-0001 <alankvickers+github@gmail.com>
2025-04-17 14:25:19 -0700

    wezterm: lower cache

--- wezterm/cfg_cache.lua
+++ wezterm/cfg_cache.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 function M.apply_to_config(config)
-  config.glyph_cache_image_cache_size = 768
-  config.line_quad_cache_size = 3072
-  config.line_state_cache_size = 3072
-  config.line_to_ele_shape_cache_size = 3072
+  config.glyph_cache_image_cache_size = 512
+  config.line_quad_cache_size = 2048
+  config.line_state_cache_size = 2048
+  config.line_to_ele_shape_cache_size = 2048
   config.mux_output_parser_buffer_size = 1000000
-  config.mux_output_parser_coalesce_delay_ms = 10
+  config.mux_output_parser_coalesce_delay_ms = 3
   config.periodic_stat_logging = 10
-  config.shape_cache_size = 3072
+  config.shape_cache_size = 2048
 end
 
 return M
