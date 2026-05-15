4f46e3791961468cf6456e7ee69260933b01708c
ayvi-0001 <alankvickers+github@gmail.com>
2026-05-15 00:12:00 -0700

    yazi(plugin): bump MasouShizuka/projects

--- yazi/init.lua
+++ yazi/init.lua
@@ -10,7 +10,15 @@ local bookmarks = os.getenv "YAZI_CONFIG_HOME" .. "/state/bookmark"
 require("yamb"):setup { jump_notify = true, cli = "fzf", path = bookmarks }
 
 local projects = os.getenv "YAZI_CONFIG_HOME" .. "/state/projects.json"
-require("projects"):setup { save = { method = "lua", lua_save_path = projects } }
+require("projects"):setup {
+  save = { method = "lua", lua_save_path = projects },
+  last = {
+        update_after_save = true,
+        update_after_load = true,
+        update_before_quit = true,
+        load_after_start = false,
+    }
+}
 
 require("copy-file-contents"):setup {}
 
