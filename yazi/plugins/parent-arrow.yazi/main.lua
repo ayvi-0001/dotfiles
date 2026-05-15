1f35d88020639885a44f972cc09becc43c4e3f7c
ayvi-0001 <alankvickers+github@gmail.com>
2026-05-15 00:05:42 -0700

    yazi(plugins): update parent-arrow @since 25.5.28

--- yazi/plugins/parent-arrow.yazi/main.lua
+++ yazi/plugins/parent-arrow.yazi/main.lua
@@ -1,13 +1,17 @@
+--- @since 25.5.28
+
 --- @sync entry
 -- https://yazi-rs.github.io/docs/tips#parent-arrow
 local function entry(_, job)
-	local parent = cx.active.parent
-	if not parent then return end
+  local parent = cx.active.parent
+  if not parent then
+    return
+  end
 
-	local target = parent.files[parent.cursor + 1 + job.args[1]]
-	if target and target.cha.is_dir then
-		ya.manager_emit("cd", { target.url })
-	end
+  local target = parent.files[parent.cursor + 1 + job.args[1]]
+  if target and target.cha.is_dir then
+    ya.emit("cd", { target.url })
+  end
 end
 
 return { entry = entry }
