46fab53c99b261dbb117e57664c9c5b365bbbe2d
ayvi-0001 <alankvickers+github@gmail.com>
2026-05-16 15:58:07 -0700

    yazi(plugins/wezyx): fix url.path

--- yazi/plugins/wezyx.yazi/main.lua
+++ yazi/plugins/wezyx.yazi/main.lua
@@ -5,6 +5,31 @@ local M = {}
 ---@type string?
 WEZTERM_PANE = os.getenv "WEZTERM_PANE"
 
+---@param content any
+---@param level "info"|"warn"|"error"|nil?
+local ya_notify = function(content, level)
+  ya.notify {
+    title = "wezyx.yazi",
+    content = tostring(content),
+    level = level,
+    timeout = 2.0,
+  }
+end
+
+---@generic T
+---@param v? T
+---@param err? any
+---@param level? integer
+---@return T
+local ya_unwrap = function(v, err, level)
+  if v == nil then
+    ya.err(err)
+    ya_notify(err, "error")
+    error(err, level)
+  end
+  return v
+end
+
 ---@overload fun(): fs__File
 local get_active_hovered = ya.sync(function()
   return cx.active.current.hovered
@@ -15,15 +40,16 @@ local get_active_selected = ya.sync(function()
   return cx.active.selected
 end)
 
+---@return string
 local get_target_paths = function()
-  local target_paths ---@type string
+  local target_paths ---@type string?
 
   local selected = get_active_selected()
   if #selected > 0 then
     local selected_paths = ""
     for _, p in pairs(selected) do
       if ya.target_family() == "windows" then
-        selected_paths = selected_paths .. ya.quote(tostring(p)) .. " "
+        selected_paths = selected_paths .. ya.quote(tostring(p.path)) .. " "
       else
         selected_paths = selected_paths .. tostring(p) .. " "
       end
@@ -32,11 +58,7 @@ local get_target_paths = function()
     target_paths = tostring(selected_paths)
   else
     local hovered_url = get_active_hovered().url
-    if not hovered_url then
-      ya.err "error: could not determine selected/hovered url(s) in yazi."
-      return
-    end
-
+    hovered_url = ya_unwrap(hovered_url, "error: could not determine selected/hovered url(s) in yazi.")
     target_paths = ya.quote(tostring(hovered_url))
   end
 
@@ -47,19 +69,6 @@ local get_target_paths = function()
   return target_paths
 end
 
----@generic T
----@param v? T
----@param msg? any
----@param level? integer
----@return T
-local ya_assert = function(v, msg, level)
-  if not v then
-    ya.err(msg)
-    error(msg, level)
-  end
-  return v
-end
-
 ---Save target urls in a file in yazi's cache_dir.
 ---If any files are selected, those will be the targeted paths.
 ---If no files are selected, then it will default to the hovered url.
@@ -68,11 +77,11 @@ end
 ---@async
 local cache_target_paths = function(body) ---@diagnostic disable-line: unused-local
   local cache_dir = rt.preview.cache_dir ---@type string?
-  cache_dir = ya_assert(cache_dir, "error: missing preview.cache_dir setting in yazi config.", 2)
+  cache_dir = ya_unwrap(cache_dir, "error: missing preview.cache_dir setting in yazi config.", 2)
 
   local cache_file = (cache_dir .. "/yazi-target-paths-wezterm-pane-" .. WEZTERM_PANE):gsub("\\", "/")
   local file = io.open(cache_file, "w")
-  file = ya_assert(file, "error: could not create/open temp file - " .. cache_file, 2)
+  file = ya_unwrap(file, "error: could not create/open temp file - " .. cache_file, 2)
 
   file:write(get_target_paths())
   file:flush()
@@ -85,7 +94,7 @@ local wezyx_remote_callback = function(body)
   -- can't determine YAZI_ID from wezterm, instead message is published
   -- to all instances and only calls back on matching wezterm pane.
   if body.wezterm_pane and tonumber(WEZTERM_PANE) == tonumber(body.wezterm_pane) then
-    ya_assert(body.fn, "error: published message to `wezyx` missing required parameter `fn`.", 2)
+    ya_unwrap(body.fn, "error: published message to `wezyx` missing required parameter `fn`.", 2)
     CALLBACKS[body.fn](body)
   end
 end
