d274ea6b3cad11e568b44fad166c9ecba28df6e9
ayvi-0001 <alankvickers+github@gmail.com>
2026-09-19 16:18:10 -0700

    wezterm: dec window_space resize_increment

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ require("window_space").setup {
   enable_window_resize_events = true,
   enable_window_move_events = true,
   enable_pane_move_events = true,
-  resize_increment = 50,
+  resize_increment = 25,
 }
 
 require "events"
